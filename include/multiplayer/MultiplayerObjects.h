4e41d0bb5d15da46f0a1d1607728c831acaac571
Chris Kennedy <cmkennedy20@gmail.com>
2020-11-22 21:49:53 -0500

    Added base functionality for client & server

--- include/multiplayer/MultiplayerObjects.h
+++ include/multiplayer/MultiplayerObjects.h
@@ -1,8 +1,66 @@
-//Needs additional information to be complete
-class turnInformation{
-    
-    int money;
-    int xPos;
-    int yPos; 
 
+//Structure that is used to define player attributes
+struct playerInfo{
+    unsigned char playerNum;    //defines which player is which
+    int money;                  //Changes in money
+    int boardNum;               //Space indicator
+    int turnCounter;
+    bool isTurn;
+}
+
+//Determines the move that each player makes
+struct playerMove{
+    unsigned char moveChoice;    //Selects which move
+}
+
+//Message sent to 
+struct boardInfo{
+    unsigned char playerCount;  //Indicator as to how many players there are in the game
+    char [4*sizeof(playerInfo)]; //Array of players information
+}
+
+//Class defined for each player
+class server
+{
+public:
+    server() = delete;
+    ~server();
+    server(unsigned short usPort);
+    void updateBoard(const std::string &strTo, unsigned short usPortNum, const playerInfo &player);
+    void addSource(const sockaddr_in &from); 
+    int sockInit(void);
+    int sockQuit(void);
+    int sockClose();
+    void error(const char *msg);
+    int m_sockfd;
+
+private:
+    unsigned short portNum;
+    std::thread recieveThread;  //Thread to receive board info
+    std::vector<playerInfo> players;
+    std::list<sockaddr_in> sources;
+    std::mutex currTurn;
+};
+
+//Class defined for each player
+class client
+{
+public:
+    client() = delete;
+    ~client();
+    client(unsigned short usPort);
+    void submitTurn(const std::string &strTo, unsigned short usPortNum, const playerMove &player);
+    void addSource(const sockaddr_in &from); 
+    int sockInit(void);
+    int sockQuit(void);
+    int sockClose();
+    void error(const char *msg);
+    int m_sockfd;
+
+private:
+    unsigned short portNum;
+    std::thread recieveThread;
+    std::vector<playerInfo> players;
+    sockaddr_in sources;
+    std::mutex currTurn;
 };
--- src/multiplayer/MultiplayerObjects.cpp
+++ src/multiplayer/MultiplayerObjects.cpp
@@ -1,4 +1,243 @@
-//Needs additional information to be complete
-class turnInformation{
-    
+#include "MultiplayerObjects.h"
+
+//Implementation for Multiplayer Classes
+
+
+/////////////////////////////////////////////////
+// Client Implementation
+/////////////////////////////////////////////////
+
+// Cross-platform socket initialize
+
+// Cross-platform socket initialize
+server::server(unsigned short usPort) : m_usPort(usPort)
+{
+    sockaddr_in serv_addr;
+    sockInit();
+    // Create the socket
+    m_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    // Make sure the socket was created
+    if (m_sockfd < 0)
+        error("ERROR opening socket");
+    // Zero out the variable serv_addr
+    memset((char *)&serv_addr, sizeof(serv_addr), 0);
+
+    // Initialize the serv_addr
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    // Convert port number from host to network
+    serv_addr.sin_port = htons(usPort);
+    // Bind the socket to the port number
+    if (bind(m_sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        error("ERROR on binding");
+    }
+    // Start thread that waits for messages
+    m_recvThread = std::thread(receiveSocketMsgs, this);
 };
+
+void server::receiveSocketMsgs(server *pUDpSocket)
+{
+    // Loop that waits on incoming messages
+    playerMove inMsg;
+    sockaddr_in from;
+    socklen_t fromlen{sizeof(struct sockaddr_in)};
+    int n;
+    do
+    {
+        n = recvfrom(pUDpSocket->m_sockfd, (char *)&inMsg, sizeof(playerMove), 0, (struct sockaddr *)&from, &fromlen);
+        if (n < 0)
+        {
+            break;
+        }
+        else
+        {
+          //Needs functional logic
+        }
+    } while (true);
+}
+
+int server::sockInit(void)
+{
+#ifdef _WIN32
+    WSADATA wsa_data;
+    return WSAStartup(MAKEWORD(1, 1), &wsa_data);
+#else
+    return 0;
+#endif
+}
+
+// Cross-platform socket quit
+int server::sockQuit(void)
+{
+#ifdef _WIN32
+    return WSACleanup();
+#else
+    return 0;
+#endif
+}
+
+// Cross-platform socket close
+int server::sockClose()
+{
+    int status = 0;
+#ifdef _WIN32
+    status = shutdown(m_sockfd, SD_BOTH);
+    if (status == 0)
+    {
+        status = closesocket(m_sockfd);
+    }
+#else
+    status = shutdown(m_sockfd, SHUT_RDWR);
+    if (status == 0)
+    {
+        status = close(m_sockfd);
+    }
+#endif
+
+    // Set to show socket closed
+    m_sockfd = -1;
+    // Wait for thread to shut down.
+    return status;
+}
+
+// Output error message and exit
+void server::error(const char *msg)
+{
+    perror(msg);
+    // Make sure any open sockets are closed before calling exit
+    sockClose();
+    exit(1);
+}
+
+//Function that adds a new source to the server function.
+void server::addSource(const sockaddr_in &from)
+{
+    bool bIsPresent = false;
+    // Iterate through list check is already present
+    for (auto i : sources)
+    {
+        if (memcmp(&i, &from, sizeof(sockaddr_in)) == 0) //Compare blocks of memory
+            bIsPresent = true;                           //If that block of memory contains the sockaddr_in then flip the bool
+    }
+    if (!bIsPresent)
+    {
+        sources.push_back(from); //Insert the sockaddr_in if it is not in the list
+    }
+}
+
+
+
+/////////////////////////////////////////////////
+// Client Implementation
+/////////////////////////////////////////////////
+
+client::client(unsigned short usPort) : m_usPort(usPort)
+{
+    sockaddr_in serv_addr;
+    sockInit();
+    // Create the socket
+    m_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    // Make sure the socket was created
+    if (m_sockfd < 0)
+        error("ERROR opening socket");
+    // Zero out the variable serv_addr
+    memset((char *)&serv_addr, sizeof(serv_addr), 0);
+
+    // Initialize the serv_addr
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    // Convert port number from host to network
+    serv_addr.sin_port = htons(usPort);
+    // Bind the socket to the port number
+    if (bind(m_sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        error("ERROR on binding");
+    }
+    // Start thread that waits for messages
+    m_recvThread = std::thread(receiveSocketMsgs, this);
+};
+
+void client::receiveSocketMsgs(client *pUDpSocket)
+{
+    // Loop that waits on incoming messages
+    boardInfo inMsg;
+    sockaddr_in from;
+    socklen_t fromlen{sizeof(struct sockaddr_in)};
+    int n;
+    do
+    {
+        n = recvfrom(pUDpSocket->m_sockfd, (char *)&inMsg, sizeof(boardInfo), 0, (struct sockaddr *)&from, &fromlen);
+        if (n < 0)
+        {
+            break;
+        }
+        else
+        {
+          //Needs functional logic
+        }
+    } while (true);
+}
+
+int client::sockInit(void)
+{
+#ifdef _WIN32
+    WSADATA wsa_data;
+    return WSAStartup(MAKEWORD(1, 1), &wsa_data);
+#else
+    return 0;
+#endif
+}
+
+// Cross-platform socket quit
+int client::sockQuit(void)
+{
+#ifdef _WIN32
+    return WSACleanup();
+#else
+    return 0;
+#endif
+}
+
+// Cross-platform socket close
+int client::sockClose()
+{
+
+    int status = 0;
+
+#ifdef _WIN32
+    status = shutdown(m_sockfd, SD_BOTH);
+    if (status == 0)
+    {
+        status = closesocket(m_sockfd);
+    }
+#else
+    status = shutdown(m_sockfd, SHUT_RDWR);
+    if (status == 0)
+    {
+        status = close(m_sockfd);
+    }
+#endif
+
+    // Set to show socket closed
+    m_sockfd = -1;
+    // Wait for thread to shut down.
+    return status;
+}
+
+// Output error message and exit
+void client::error(const char *msg)
+{
+    perror(msg);
+    // Make sure any open sockets are closed before calling exit
+    sockClose();
+    exit(1);
+}
+
+//Function that adds a new source to the server function.
+void client::addSource(const sockaddr_in &from)
+{
+    bool bIsPresent = false;
+    sources = from;
+    // Iterate through list check is already present
+}
