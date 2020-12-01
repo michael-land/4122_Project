48887bd4847b8075ce9a8f03a966b18509fa4ed0
cmkennedy20 <60405484+cmkennedy20@users.noreply.github.com>
2020-12-01 17:49:39 -0500

    Update BoardSpace.h

--- include/gamerules/BoardSpace.h
+++ include/gamerules/BoardSpace.h
@@ -1,10 +1,8 @@
-#ifndef BOARDSPACE_H
-#define BOARDSPACE_H
-
-#include <string>
-#include <vector>
-class Player;
 /*
+Authors: Christopher Kennedy, Jackson Stanhope, Jim O'Donnell, Michael Zhou Lu, Ruben Quiros, and Shelby Crisp 
+Class: ECE 4122
+Last Date Modified: 12/1/20
+Description:
  * This class is essentially a linked list node for
  * the 'Board' class, which contains implementation
  * for the game board as a linked list.
@@ -12,28 +10,36 @@ class Player;
  * property that players can own/have to pay rent to on the
  * board tile, and a vector containing pointers
  * to player objects that are currently at that tile.
- */
+*/
+
+#ifndef BOARDSPACE_H
+#define BOARDSPACE_H
+
+#include <string>
+#include <vector>
+class Player;
+
 class BoardSpace {
     private:
-    int spaceID;
-    std::string name;
-    BoardSpace* nextSpace;
-    BoardSpace* prevSpace;
-    std::vector<Player*> playersOnSpace;
-    int improvements; // this is for houses, hotels, etc.
+    int spaceID;    //ID for that given boardspace
+    std::string name;   //Name of that space
+    BoardSpace* nextSpace;  //Pointer to the next space
+    BoardSpace* prevSpace;  //Pointer to the previous space
+    std::vector<Player*> playersOnSpace;    //Vector of players on that given space
+    int improvements; // this is for houses, hotels, etc.  
 
     public:
-    BoardSpace(std::string);
-    std::string getName() const;
-    void setName(const std::string&);
-    BoardSpace* getNextSpace() const;
-    BoardSpace* getPrevSpace() const;
-    void setNextSpace(BoardSpace*);
-    void setPrevSpace(BoardSpace*);
-    void setSpaceID(const int&);
-    int getSpaceID() const;
-    ~BoardSpace();
-    virtual void placeholder();
+    BoardSpace(std::string);    //Constructor being passed a string name
+    std::string getName() const;    //Returns the name of the boardspace
+    void setName(const std::string&);   //Sets the name for the boardspace
+    BoardSpace* getNextSpace() const;   //Returns a pointer to the next board space
+    BoardSpace* getPrevSpace() const;   //Returns a pointer to the previous boardspace
+    void setNextSpace(BoardSpace*);     //Sets the next board space to the pointer passed
+    void setPrevSpace(BoardSpace*);     //Sets the previous board space to the pointer passed
+    void setSpaceID(const int&);    //Sets the space id with the int passed
+    int getSpaceID() const;     //Returns the space id 
+    ~BoardSpace();  //Destructor
+    virtual void placeholder(); 
 
 };
 
